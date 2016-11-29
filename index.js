6fee8234d76aee27402845eab452f2d6e2434d5a
Bibi Khadiza <limakhadiza@yahoo.com>
2016-11-29 17:13:57 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+
+function Rectangle(x, y, width, height) {
+  Shape.call(this, 4, x, y);
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+Rectangle.prototype.perimeter = function () {
+  return (this.width + this.height) * 2;
+}
